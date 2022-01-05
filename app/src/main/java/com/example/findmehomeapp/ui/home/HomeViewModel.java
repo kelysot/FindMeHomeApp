918a6b50f32469a460e34b492c10ff246ddee998
shirelronis <shirelbar100@gmail.com>
2022-01-05 14:00:02 +0200

    set homeViewModel to be for posts livedata

--- app/src/main/java/com/example/findmehomeapp/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/findmehomeapp/ui/home/HomeViewModel.java
@@ -4,16 +4,20 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.findmehomeapp.Model.Model;
+import com.example.findmehomeapp.Model.Post;
+
+import java.net.HttpCookie;
+import java.util.List;
+
 public class HomeViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
+    LiveData<List<Post>> data;
 
-    public HomeViewModel() {
-        mText = new MutableLiveData<>();
-        //mText.setValue("This is home fragment");
+    public HomeViewModel(){
+        data = Model.instance.getAllPosts();
     }
-
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<List<Post>> getData() {
+        return data;
     }
 }
