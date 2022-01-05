70fda51ff3fca84f82e2688758dc0823c67ccac0
shirelronis <shirelbar100@gmail.com>
2022-01-05 14:33:22 +0200

    let home fragment a bit work with view model

--- app/src/main/java/com/example/findmehomeapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/findmehomeapp/ui/home/HomeFragment.java
@@ -1,9 +1,14 @@
 package com.example.findmehomeapp.ui.home;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,36 +16,145 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import com.example.findmehomeapp.Model.Model;
+import com.example.findmehomeapp.Model.Post;
 import com.example.findmehomeapp.R;
 import com.example.findmehomeapp.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
-    private FragmentHomeBinding binding;
+    MyAdapter adapter;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        //final TextView textView = binding;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                //textView.setText(s);
-            }
-        });
-        return root;
-    }
+    SwipeRefreshLayout swipeRefresh;
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
     }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home,container,false);
+
+        swipeRefresh = view.findViewById(R.id.postslist_swiperefresh);
+        swipeRefresh.setOnRefreshListener(() -> Model.instance.refreshPostsList());
+
+        RecyclerView list = view.findViewById(R.id.home_post_rv) ;
+        list.setHasFixedSize(true);
+
+        list.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        adapter = new MyAdapter();
+        list.setAdapter(adapter);
+
+        adapter.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(View v, int position) {
+                String postId = homeViewModel.getData().getValue().get(position).getId();
+                Navigation.findNavController(v).navigate(HomeFragmentDirections.actionNavHomeToNavPost(postId));
+            }
+        });
+
+        setHasOptionsMenu(true);
+        homeViewModel.getData().observe(getViewLifecycleOwner(), list1 -> refresh());
+
+        swipeRefresh.setRefreshing(Model.instance.getPostListLoadingState().getValue() == Model.PostListLoadingState.loading);
+
+        Model.instance.getPostListLoadingState().observe(getViewLifecycleOwner(), new Observer<Model.PostListLoadingState>() {
+            @Override
+            public void onChanged(Model.PostListLoadingState postListLoadingState) {
+                if(postListLoadingState == Model.PostListLoadingState.loading) {
+                    swipeRefresh.setRefreshing(true);
+                } else {
+                    swipeRefresh.setRefreshing(false);
+                }
+            }
+        });
+
+        return view;
+    }
+
+    private void refresh() {
+        adapter.notifyDataSetChanged();
+        swipeRefresh.setRefreshing(false);
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView nameTv;
+        TextView idTv;
+        CheckBox cb;
+
+        public MyViewHolder(@NonNull View itemView, OnItemClickListener listener) {
+            super(itemView);
+//            nameTv = itemView.findViewById(R.id.listrow_name_tv);
+//            idTv = itemView.findViewById(R.id.listrow_id_tv);
+//            cb = itemView.findViewById(R.id.listrow_cb);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int pos = getAdapterPosition();
+                    listener.onItemClick(v, pos);
+                }
+            });
+        }
+    }
+
+    interface OnItemClickListener{
+        void onItemClick(View v, int position);
+    }
+    class MyAdapter extends RecyclerView.Adapter<MyViewHolder>{
+
+        OnItemClickListener listener;
+        public void setOnItemClickListener(OnItemClickListener listener){
+            this.listener = listener;
+        }
+
+        @NonNull
+        @Override
+        public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = getLayoutInflater().inflate(R.layout.post_list_row,parent,false);
+            MyViewHolder holder = new MyViewHolder(view,listener);
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+            Post post = homeViewModel.getData().getValue().get(position);
+            //TODO: set relevants from holder
+//            holder.nameTv.setText(student.getName());
+//            holder.idTv.setText(student.getId());
+        }
+
+        @Override
+        public int getItemCount() {
+            if(homeViewModel.getData().getValue() == null) {
+                return 0;
+            }
+            return homeViewModel.getData().getValue().size();
+        }
+    }
+
+//    @Override
+//    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+//        super.onCreateOptionsMenu(menu, inflater);
+//        inflater.inflate(R.menu.student_list_menu, menu);
+//    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+//        if (item.getItemId() == R.id.addStudentFragment) {
+//            return true;
+//        } else {
+//            return super.onOptionsItemSelected(item);
+//        }
+//    }
 }
