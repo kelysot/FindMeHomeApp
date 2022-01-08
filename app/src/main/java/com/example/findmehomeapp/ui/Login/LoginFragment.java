00367735e27730c55e5901a400943da3be6e4f22
shirelronis <shirelbar100@gmail.com>
2022-01-08 23:19:41 +0200

    open profile from login page (without validation)

--- app/src/main/java/com/example/findmehomeapp/ui/Login/LoginFragment.java
+++ app/src/main/java/com/example/findmehomeapp/ui/Login/LoginFragment.java
@@ -12,26 +12,34 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.findmehomeapp.R;
 
 public class LoginFragment extends Fragment {
-    Button registerBtn;
+    EditText emailEt;
+    EditText passwordEt;
+    Button loginBtn;
     TextView registerTv;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_login, container, false);
-        registerBtn = view.findViewById(R.id.login_btn_login);
+
+        emailEt = view.findViewById(R.id.login_et_email);
+        passwordEt = view.findViewById(R.id.login_et_password);
+
+        loginBtn = view.findViewById(R.id.login_btn_login);
         registerTv = view.findViewById(R.id.login_register_page);
 
         registerTv.setOnClickListener((v)-> {
             Navigation.findNavController(v).navigate(LoginFragmentDirections.actionNavLoginToNavRegister());
         });
-        registerBtn.setOnClickListener((v)-> {
-//            Navigation.findNavController(v).navigate(LoginFragmentDirections.actionNavLoginToNavRegister());
+        loginBtn.setOnClickListener((v)-> {
+            //TODO: valid user email and password
+            Navigation.findNavController(v).navigate(LoginFragmentDirections.actionGlobalNavProfile(emailEt.getText().toString()));
         });
 
         return view;
