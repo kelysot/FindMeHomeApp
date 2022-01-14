ddc6edc5cbbd875e5d2748a94085a57df7d305f7
kelysot <kelysot@gmail.com>
2022-01-13 16:38:44 -0800

    Reduce code in register and profile fragments. Now the user's profile picture is displayed in the profile. Transfer all the code of the firebase from the register fragment to the firebase model.

--- app/src/main/java/com/example/findmehomeapp/ui/Profile/ProfileFragment.java
+++ app/src/main/java/com/example/findmehomeapp/ui/Profile/ProfileFragment.java
@@ -9,12 +9,16 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.findmehomeapp.Model.Model;
+import com.example.findmehomeapp.Model.User;
 import com.example.findmehomeapp.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -22,6 +26,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.squareup.picasso.Picasso;
 
 
 public class ProfileFragment extends Fragment {
@@ -29,14 +34,15 @@ public class ProfileFragment extends Fragment {
     TextView nameTv;
     TextView locationTv;
     TextView phoneTv;
-    //ImageView avatarImv;
+    ImageView avatarImv;
     Button addPostBtn;
     Button editProfileBtn;
     RecyclerView postList;
     FirebaseFirestore firestore;
     FirebaseAuth firebaseAuth;
     NavController navController;
-    String userid;
+    String userId;
+
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -46,26 +52,35 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+//        String stId = ProfileFragmentArgs.fromBundle(getArguments()).getUserId();
+        userId = firebaseAuth.getCurrentUser().getUid();
+        Log.d("TAG1", "user Id:" + userId );
+
+
+        Model.instance.getUserById(userId, new Model.GetUserById() {
+            @Override
+            public void onComplete(User user) {
+                Log.d("TAG111", "user Id:" + user.getId() );
+                nameTv.setText(user.getName());
+                phoneTv.setText(user.getPhone());
+                locationTv.setText(user.getLocation());
+                if (user.getAvatarUrl() != null) {
+                    Picasso.get().load(user.getAvatarUrl()).into(avatarImv);
+                }
+            }
+        });
 
         nameTv = view.findViewById(R.id.profile_user_name);
         phoneTv = view.findViewById(R.id.profile_user_phone);
         locationTv = view.findViewById(R.id.profile_user_location);
+        avatarImv = view.findViewById(R.id.profile_image);
 
         addPostBtn = view.findViewById(R.id.profile_btn_add_post);
         editProfileBtn = view.findViewById(R.id.profile_btn_edit_profile);
-
-        navController = Navigation.findNavController(view);
-
-
-
-        firebaseAuth = FirebaseAuth.getInstance();
-        firestore = FirebaseFirestore.getInstance();
 
         addPostBtn.setOnClickListener((v)->{
             navController.navigate(R.id.action_nav_profile_to_nav_create_post);
@@ -74,66 +89,7 @@ public class ProfileFragment extends Fragment {
         editProfileBtn.setOnClickListener((v)->{
             navController.navigate(R.id.action_nav_profile_to_nav_edit_profile);
         });
-
-
-//        String userId = firebaseAuth.getCurrentUser().getUid();
-//
-//        Log.d("TAG1", "user Id:" + userId );
-//
-//        Model.instance.getUserById(userId, new Model.GetUserById() {
-//            @Override
-//            public void onComplete(User user) {
-//                Log.d("TAG2", "user Id:" + userId );
-//                nameTv.setText(user.getName());
-//                emailTv.setText(user.getEmail());
-//                phoneTv.setText(user.getPhone());
-////                if (user.getAvatarUrl() != null) {
-////                    Picasso.get().load(user.getAvatarUrl()).into(avatarImv);
-////                }
-//            }
-//        });
-
-
-
-
+        return view;
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-
-        String theuserid = user.getUid();
-
-
-        firestore.collection("Users").document(theuserid).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-
-                if (task.isSuccessful()) {
-
-                    DocumentSnapshot ssnahot = task.getResult();
-
-                    String userName = ssnahot.getString("username");
-                    String userId = ssnahot.getString("userid");
-                    String userPhone = ssnahot.getString("phone");
-                    String userLocation = ssnahot.getString("location");
-                    //imageUrl = ssnahot.getString("imageUrl");
-
-                    nameTv.setText(userName);
-                    phoneTv.setText(userPhone);
-                    locationTv.setText(userLocation);
-                   // emailTv.setText();
-
-                    //Glide.with(getContext()).load(imageUrl).centerCrop().into(circleImageView);
-
-
-
-                }
-
-            }
-        });
-    }
 }
