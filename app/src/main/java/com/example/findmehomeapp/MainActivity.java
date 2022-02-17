cf4403ed646d9a001f97786ef541d4e1d45b7a07
shirelronis <shirelbar100@gmail.com>
2022-02-17 19:28:20 +0200

    remove back button

--- app/src/main/java/com/example/findmehomeapp/MainActivity.java
+++ app/src/main/java/com/example/findmehomeapp/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_profile, R.id.nav_edit_profile)
+                R.id.nav_home, R.id.nav_profile, R.id.nav_edit_profile, R.id.nav_login)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
