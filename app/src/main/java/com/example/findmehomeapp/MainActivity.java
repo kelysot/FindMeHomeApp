cf331fbd87117b38531f6d1fcafcf0eff36f166c
kelysot <kelysot@gmail.com>
2022-03-16 11:15:29 +0200

    Delete edit profile from drawer.

--- app/src/main/java/com/example/findmehomeapp/MainActivity.java
+++ app/src/main/java/com/example/findmehomeapp/MainActivity.java
@@ -28,8 +28,9 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(binding.appBarMain.toolbar);
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
+
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_profile, R.id.nav_edit_profile)
+                R.id.nav_home, R.id.nav_profile)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
